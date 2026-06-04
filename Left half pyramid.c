bd7649fef1299f3a76914a7d961db1a6ab8456b4
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-04 20:42:09 +0600

    Add files via upload

---
+++ Left half pyramid.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("Left half pyramid\n");
+    printf("    *\n");
+    printf("   **\n");
+    printf("  ***\n");
+    printf(" ****\n");
+    printf("*****\n");
+    return 0;
+}
