d2ea5cace191bc7838ea8dafa307bdfafb25e65d
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-04 20:38:30 +0600

    Add files via upload

---
+++ two integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int first,second;
+    printf("Please enter the first value:");
+    scanf("%d", &first);
+
+    printf("Now, enter the second value:");
+    scanf("%d", &second);
+
+    printf("The two integers are %d and %d", first,second);
+    return 0;
+}
