bb8c528158bc94b764985bb1d2954b27637f753e
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-05 17:58:08 +0600

    Add files via upload

---
+++ escape.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("Tasnim\n Farabi\n");
+    printf("Tasnim\t Farabi\n");
+    printf("Tasnim\b Farabi\n");
+    printf("Tasnim \"Farabi\"\n");
+    printf("Tasnim \'Farabi\'\n");
+    printf("Tasnim \\Farabi\\");
+    return 0;
+}
