b403ae458da52aa37001f35f58ec3eee93d1188d
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-05 17:47:21 +0600

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int side;
+    printf("Please enter the side of square in cm: ");
+    scanf("%d", &side);
+    printf("The area of your square is %d cm*cm", side*side );
+    return 0;
+}
