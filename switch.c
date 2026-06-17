d8edd959ea58d55aed84104110783f5bcf25b732
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-17 12:45:27 +0600

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Please enter day number between (1-7): ");
+    scanf("%d", &day);
+
+    switch (day){
+     case 1: printf("Today is Monday.");
+     break;
+     case 2: printf("Today is Tuesday.");
+     break;
+     case 3: printf("Today is Wednesday.");
+     break ;
+     case 4: printf("Today is Thursday.");
+     break ;
+     case 5: printf("Today is Friday.");
+     break;
+     case 6: printf("Today is Saturday.");
+     break;
+     case 7: printf("Today is Sunday.");
+     break;
+     default:
+     printf("Day entered is incorrect.");
+    }
+    printf("\nDay calculation is complete.");
+
+    return 0;
+}
