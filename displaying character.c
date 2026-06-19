60a3d7ec61f4826158ae008bfd5e3b1522436332
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-20 04:14:13 +0600

    Add files via upload

---
+++ displaying character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char first;
+    printf("Please enter the character:");
+    scanf("%c", &first);
+
+    printf("The displayed character will be : %c", first);
+    return 0;
+}
