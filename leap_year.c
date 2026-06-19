ab94c5c1447a48a7d4989dc638e5a04f0a2525aa
tasnimfarabi48-ui <tasnimfarabi48@gmail.com>
2026-06-20 00:58:50 +0600

    Add files via upload

---
+++ leap_year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Please enter the year:");
+    scanf(" %d", &year);
+
+    if(year % 400 ==0)
+    {
+        printf("It is a leap year.");
+    }
+    else if(year % 100 ==0)
+    {
+        printf("It`s not a leap year");
+    }
+    else if(year % 4 == 0)
+    {
+        printf("It`s a leap year");
+    }else
+    {
+        printf("It`s not a leap year");
+    }
+    return 0;
+
+}
